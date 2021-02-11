338268a33ba62160c7c2117ab96c8e281c57ae74
Sanket Agrawal <62806894+babasanku@users.noreply.github.com>
2021-02-12 01:06:24 +0530

    Gif done

--- code.R
+++ code.R
@@ -1,20 +1,47 @@
+#### Required packages: stats, ggplot2, ggpanimate, gifski, png, transformr
+
 library(stats)
+library(gganimate)
+set.seed(4)
 
-t <- seq(0, 1, length.out = 1e3)
+# This code visualizes classical Donsker's Theorem for random samples from U(0, 1) distribution.
+# Gif shows the convergence of sqrt(n)(F_n(t) - t) to a Brownian bridge process as n increases.
+# Here F_n(.) is the empirical distribution function based on a random sample of size n.
 
-N <- c(seq(1:10), seq(20, 100, by = 10), seq(200, 1000, by = 100), seq(2e3, 1e4, by = 1e3))
-output <- matrix(0, nrow = length(N), ncol = length(t))
+t <- seq(0, 1, length.out = 1e3)  # x-axis
+
+N <- c(seq(1:10), seq(20, 100, by = 10), seq(200, 1000, by = 100), seq(2e3, 1e4, by = 1e3)) # Different values of n (sample size)
+output <- matrix(0, nrow = length(N), ncol = length(t)) # Matrix to contain process values
 
 prev <- 0
 Z <- numeric()
 for(i in 1:length(N)){
   n <- N[i] - prev
-  Z <- c(Z, runif(n, min = 0, max = 1))
+  Z <- c(Z, runif(n, min = 0, max = 1)) 
 
   cdf <- ecdf(Z)
   fn_cap <- cdf(t)
   y <- sqrt(N[i])*(fn_cap - t)
 
-  prev <- n
+  prev <- N[i]
   output[i, ] <- y
 }
+
+#################################################
+#################################################
+# For animation
+
+T <- rep(t, length(N))
+n <- factor(rep(N, each = 1e3))
+value <- numeric()
+
+for(i in 1:length(N)){
+  value <- c(value, output[i, ])
+}
+
+data <- data.frame(T, n, value)
+
+p <- ggplot(data, aes(x = T, y = value)) + geom_path()
+anim <- p + transition_states(n, transition_length = 1, state_length = 2, wrap = TRUE)
+anim <- anim + ggtitle('N = {closest_state}')
+anim
